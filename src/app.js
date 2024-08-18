8e39350221c9625c389cd6a541a639a61fcb0b63
Marcio Rodrigo <dev.marciorodrigo@gmail.com>
2024-08-19 00:23:08 +0200

    update app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import Navbar from './components/Navbar.js';
-import router from './router.js';
-import './styles/reset.css';
-import './styles/main.css';
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
+import router from "./router.js";
+import "./styles/reset.css";
+import "./styles/main.css";
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.insertAdjacentHTML('afterbegin', Navbar()); 
-  router(); 
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.insertAdjacentHTML("afterbegin", Navbar());
+  router();
+  document.body.insertAdjacentHTML("beforeend", Footer());
 });
